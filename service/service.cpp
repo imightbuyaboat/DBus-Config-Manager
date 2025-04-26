1e8aae08a590e8954094ffe6e685fee6dba30a88
imightbuyaboat <vovkbp@gmail.com>
2025-04-26 18:01:19 +0300

    fix: добавлена обработка ошибок DBus

--- service/service.cpp
+++ service/service.cpp
@@ -4,15 +4,20 @@ void ApplicationConfigObject::ChangeConfiguration(const std::string& key,
                                                   const sdbus::Variant& value) {
     dict[key] = value;
 
+    // сохраняем измененную конфигурацию в файл
+    try {
+        SaveConfiguration();
+    } catch (const std::exception& e) {
+        throw sdbus::Error(sdbus::Error::Name("com.system.configurationManager.Error"),
+                           "Failed to save configuration: " + std::string(e.what()));
+    }
+
     // создаем и отправляем сигнал об изменении настроек
     const char* interfaceName = "com.system.configurationManager.Application.Configuration";
     const char* signalName = "configurationChanged";
-    auto signal = object->createSignal(sdbus::InterfaceName(interfaceName),
-                                       sdbus::SignalName(signalName));
-    signal << dict;
+    auto signal =
+        object->createSignal(sdbus::InterfaceName(interfaceName), sdbus::SignalName(signalName));
     object->emitSignal(signal);
-
-    SaveConfiguration();
 }
 
 std::map<std::string, sdbus::Variant> ApplicationConfigObject::GetConfiguration() const {
@@ -21,29 +26,34 @@ std::map<std::string, sdbus::Variant> ApplicationConfigObject::GetConfiguration(
 
 void ApplicationConfigObject::SaveConfiguration() {
     std::ofstream file(path);
-    if (file.is_open()) {
-        Json::Value root;
+    if (!file.is_open()) {
+        throw std::runtime_error("Failed to open file: " + path);
+    }
 
-        // формируем json с настройками конфигураций приложения
-        for (const auto& [key, value] : dict) {
-            try {
-                if (value.containsValueOfType<std::string>()) {
-                    root[key] = value.get<std::string>();
-                } else if (value.containsValueOfType<uint32_t>()) {
-                    root[key] = value.get<uint32_t>();
-                } else if (value.containsValueOfType<int32_t>()) {
-                    root[key] = value.get<int32_t>();
-                } else if (value.containsValueOfType<bool>()) {
-                    root[key] = value.get<bool>();
-                } else {
-                    std::cerr << "Unknown type: " << key << std::endl;
-                }
-            } catch (const std::bad_cast& e) {
-                std::cerr << "Failed to extract value for key " << key << ": " << e.what()
-                          << std::endl;
+    Json::Value root;
+
+    // формируем json с настройками конфигураций приложения
+    for (const auto& [key, value] : dict) {
+        try {
+            if (value.containsValueOfType<std::string>()) {
+                root[key] = value.get<std::string>();
+            } else if (value.containsValueOfType<uint32_t>()) {
+                root[key] = value.get<uint32_t>();
+            } else if (value.containsValueOfType<int32_t>()) {
+                root[key] = value.get<int32_t>();
+            } else if (value.containsValueOfType<bool>()) {
+                root[key] = value.get<bool>();
+            } else {
+                std::cerr << "Unknown type: " << key << std::endl;
             }
+        } catch (const std::bad_cast& e) {
+            std::cerr << "Failed to extract value for key " << key << ": " << e.what() << std::endl;
         }
-        file << root;
+    }
+
+    file << root;
+    if (!file) {
+        throw std::runtime_error("Failed to write configuration in file: " + path);
     }
 }
 
@@ -60,7 +70,7 @@ void ApplicationConfigObject::ReadConfiguration(const std::string filePath) {
     for (const auto& key : root.getMemberNames()) {
         const Json::Value& val = root[key];
 
-        std::cout << key << ": " << val << std::endl;
+        std::cout << "\t" << key << ": " << val << std::endl;
 
         if (val.isUInt()) {
             dict[key] = sdbus::Variant(static_cast<uint32_t>(val.asUInt()));
@@ -79,24 +89,29 @@ void ApplicationConfigObject::ReadConfiguration(const std::string filePath) {
 ApplicationConfigObject::ApplicationConfigObject(std::unique_ptr<sdbus::IObject> obj,
                                                  const std::string& filePath)
     : object(std::move(obj)), path(filePath) {
-    
     const char* interfaceName = "com.system.configurationManager.Application.Configuration";
 
     // регистрируем метод ChangeConfiguration
     const char* methodName = "ChangeConfiguration";
-    object->addVTable(sdbus::registerMethod(sdbus::MethodName(methodName)).implementedAs([this](const std::string& key, const sdbus::Variant& value) {
-        return this->ChangeConfiguration(key, value);
-    })).forInterface(sdbus::InterfaceName(interfaceName));
+    object
+        ->addVTable(sdbus::registerMethod(sdbus::MethodName(methodName))
+                        .implementedAs([this](const std::string& key, const sdbus::Variant& value) {
+                            return this->ChangeConfiguration(key, value);
+                        }))
+        .forInterface(sdbus::InterfaceName(interfaceName));
 
     // регистрируем метод GetConfiguration
     methodName = "GetConfiguration";
-    object->addVTable(sdbus::registerMethod(sdbus::MethodName(methodName)).implementedAs([this]() { 
-        return this->GetConfiguration(); 
-    })).forInterface(sdbus::InterfaceName(interfaceName));
+    object
+        ->addVTable(sdbus::registerMethod(sdbus::MethodName(methodName)).implementedAs([this]() {
+            return this->GetConfiguration();
+        }))
+        .forInterface(sdbus::InterfaceName(interfaceName));
 
     // регистрируем сигнал configurationChanged
     const char* signalName = "configurationChanged";
-    object->addVTable(sdbus::registerSignal(sdbus::SignalName(signalName))).forInterface(sdbus::InterfaceName(interfaceName));
+    object->addVTable(sdbus::registerSignal(sdbus::SignalName(signalName)))
+        .forInterface(sdbus::InterfaceName(interfaceName));
 
     // читаем файл конфигурации приложения
     ReadConfiguration(filePath);
@@ -115,7 +130,8 @@ void initObjects(std::map<std::string, std::unique_ptr<ApplicationConfigObject>>
 
                 try {
                     // создаем объект DBus для текущего приложения
-                    std::string objectPath = "/com/system/configurationManager/Application/" + applicationName;
+                    std::string objectPath =
+                        "/com/system/configurationManager/Application/" + applicationName;
                     auto object = sdbus::createObject(conn, sdbus::ObjectPath(objectPath));
 
                     // создаем объект приложения
